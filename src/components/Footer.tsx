2aca74ad571b07d1123eeb54ee0feee053d6ad9a
Andrew Alexander <adrw@squareup.com>
2019-10-24 11:32:59 -0400

    Add andrew.fm link to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,25 +1,26 @@
-import { Link } from "gatsby"
-import * as React from "react"
-import styled from "react-emotion"
-import { Container, Page } from "../components"
+import { Link } from "gatsby";
+import * as React from "react";
+import styled from "react-emotion";
+import { Container, Page } from "../components";
 
 interface FooterProps {
-  className?: string
+  className?: string;
 }
 
 const StyledFooter = styled.div`
   margin: 10% 0;
-`
+`;
 
 const Footer: React.SFC<FooterProps> = ({ className }) => (
   <Container>
     <Page>
       <StyledFooter className={className}>
         &copy; <Link to="/">ADRW Digital</Link> 2009-
-        {new Date().getFullYear()}
+        {new Date().getFullYear()} |{" "}
+        <a href="https://andrew.fm/">Andrew Alexander</a>
       </StyledFooter>
     </Page>
   </Container>
-)
+);
 
-export { Footer }
+export { Footer };
